09a228d13a3336323f5b46a2adae76ded3261aca
Abhishek Jujjuvarapu <abhishek.j3094@gmail.com>
2026-01-20 01:01:39 +0530

    working with admin panel features implementation

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './logo.png'
+const logo = '/main-logo.jpeg'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='footer' id='footer'>
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.logo} alt="" />
+            <img className="footer-logo" src={assets.logo} alt="" />
             <p>Fresh meals delivered fast, with real-time order tracking and secure checkout. Explore curated menus, place orders in seconds, and enjoy reliable delivery right to your door.</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
