cbe3b68559f5fdfa2985a4e62c8553900c02919a
Abhishek Jujjuvarapu <abhishek.j3094@gmail.com>
2026-01-24 12:29:58 +0530

    first commit

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://bite-buzz-backend-43lu.onrender.com"
     const [food_list, setFoodList] = useState(menuData.food_list);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
