cc95e193ad22e6c6b7ebb07cd6b6eca28b9f624d
Abhishek Jujjuvarapu <abhishek.j3094@gmail.com>
2026-01-26 22:49:21 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
-
     try {
         await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to MongoDB");
